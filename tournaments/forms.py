6f83709a7e46f8e0e4b0c0d0e25203975f5407fe
dina <dinaaabiakar@gmail.com>
2026-04-12 17:47:03 +0300

    Fixed UI

--- tournaments/forms.py
+++ tournaments/forms.py
@@ -1,12 +1,13 @@
 from django import forms
 from .models import Tournament
 
-
 class TournamentForm(forms.ModelForm):
     class Meta:
         model = Tournament
         fields = ['name', 'start_date', 'end_date', 'format']
         widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Enter tournament name'}),
             'start_date': forms.DateInput(attrs={'type': 'date'}),
             'end_date': forms.DateInput(attrs={'type': 'date'}),
+            'format': forms.Select(),
         }
