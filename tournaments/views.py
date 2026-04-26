dde02d2e56e9629bb67ec15dc97d02aaff28db03
NourAawar <aawarnour@gmail.com>
2026-04-27 00:55:30 +0300

    Dockerize + announcement notification fixes

--- tournaments/views.py
+++ tournaments/views.py
@@ -917,6 +917,7 @@ def send_team_message(request, team_id):
                 Notification.objects.create(
                     user_id=pid,
                     message=f"New message from coach {request.user.username}: {announcement.title}",
+                    announcement = announcement, 
                 )
 
             messages.success(request, "Message sent to team successfully.")
@@ -948,6 +949,7 @@ def send_announcement(request):
                 Notification.objects.create(
                     user=user,
                     message=f"System announcement: {announcement.title}",
+                    announcement = announcement, 
                 )
 
             messages.success(request, "Announcement sent to all users.")
